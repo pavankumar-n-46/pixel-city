020aec38ca7570e8af13feb2df6f3154e4e825a8
pavan kumar N <pavan.n@allegion.com>
2019-01-22 15:47:19 +0530

    add assets and build UI

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self 
     }
-
-
+    
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+    }
+    
 }
 
+extension MapVC: MKMapViewDelegate{
+    
+}
